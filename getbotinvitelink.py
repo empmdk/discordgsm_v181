f329b8181d188ff0424ddb582c7668ee61eecbb4
empmdk <empmdk@gmail.com>
2022-02-14 00:19:43 -0500

    Initial Dump
    Initial Dump

---
+++ getbotinvitelink.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+from settings import Settings
+
+bot = discord.Client()
+
+settings = Settings.get()
+TOKEN = os.getenv('DGSM_TOKEN', settings['token'])
+
+@bot.event
+async def on_ready():
+    print(f'https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=268954704&scope=bot')
+    await bot.close()
+
+try:
+    bot.run(TOKEN)
+except:
+    pass
